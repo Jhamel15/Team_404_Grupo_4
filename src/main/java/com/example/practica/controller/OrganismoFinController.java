e3660ce8bd296e42038cb3ae4e0a5a2cbd63400e
sebasanze10 <sebastianalvaroanzecolque@gmail.com>
2026-05-22 05:15:08 -0400

    Subo mi parte de mi tabla (Estados Financieros)

--- src/main/java/com/example/practica/controller/OrganismoFinController.java
+++ src/main/java/com/example/practica/controller/OrganismoFinController.java
@@ -4,7 +4,6 @@ import com.example.practica.Entity.OrganismoFin;
 import com.example.practica.services.OrganismoFinService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
