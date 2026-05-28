a63411cf6ffebf9fca0ad7327b841b791b6dd00c
ubaldesjavier@gmail.com <ubaldesjavier@gmail.com>
2026-05-28 12:23:50 -0400

    Arregalndo el valor nulo de Cors

--- src/main/java/com/example/practica/Config/CorsConfig.java
+++ src/main/java/com/example/practica/Config/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.example.practica.Config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
